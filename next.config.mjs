21925e0070e6bec7d8a375997abaed65d9b708c4
Yogesh Nandal <yogidesign1996@gmail.com>
2025-08-07 12:09:56 +0530

    Update next.config.mjs to enable standalone output and standardize image remote patterns for improved consistency.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
-        port: '',
-        pathname: '/vi/**',
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+        pathname: "/vi/**",
       },
     ],
   },
